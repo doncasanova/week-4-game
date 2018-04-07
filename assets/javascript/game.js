311c7da2dffae58da0c5ff38305787b899643d36
doncasanova <ethanjuan@yahoo.com>
2018-04-07 07:48:30 -0500

    added respose to cyrstals and text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,7 @@ for (var i = 0; i < 4; i++) {
     var numberOptions = 1 + Math.floor(Math.random() * 11)
     console.log(numberOptions + "  crystal")
     var imageCrystal = $("<img>");
-    imageCrystal.addClass("crystal-image");
+    imageCrystal.addClass("crystal-image crystal-image2 crystal-image3 ");
     imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
     imageCrystal.attr("data-crystalvalue", numberOptions);
     crystals.append(imageCrystal);
@@ -25,12 +25,13 @@ $(document).ready(function () {
         document.getElementById("user-guess").innerHTML = counter;
         document.getElementById("re-start").innerHTML = "Play Again";
         //-------------------------------------------------------------------------
-        document.getElementById("instructions").innerHTML = "";
+        document.getElementById("instructions").innerHTML = "May the universe be with you ";
         //-----------------------------------------------------------------------------
         //picks random numbers to add to crystals
 
         for (var i = 0; i < 4; i++) {
             var numberOptions = 1 + Math.floor(Math.random() * 11)
+            console.log(numberOptions + "  crystal")
             var imageCrystal = $("<img>");
             imageCrystal.attr("data-crystalvalue", numberOptions);
             crystals.append(imageCrystal);
@@ -44,44 +45,43 @@ $(document).ready(function () {
         //------------------------------------------------------------------------------
         //adds players guess together
         // $(document).ready(function () {
-            $("#crystals").on("click", ".crystal-image", function () {
-                var crystalValue = ($(this).attr("data-crystalvalue"));
-                crystalValue = parseInt(crystalValue);
-                counter += crystalValue;
-                // alert("New score: " + counter);
-                document.getElementById("user-guess").innerHTML = "" + counter;
+        $("#crystals").on("click", ".crystal-image", function () {
+            var crystalValue = ($(this).attr("data-crystalvalue"));
+            crystalValue = parseInt(crystalValue);
+            counter += crystalValue;
+            // alert("New score: " + counter);
+            document.getElementById("user-guess").innerHTML = "" + counter;
 
-                if (counter === targetNumber) {
-                    setTimeout(function () {
-                        alert("You win!");
-                    }, 100);
-                    
-                    // window.location.reload()
-                    wins++
-                    // $("wins").text("Wins <br/>  " + wins)
-                    document.getElementById("wins").innerHTML = "Wins <br/> " + wins;
-                    $("#crystals").off("click", ".crystal-image")
-                    
-                }
+            if (counter === targetNumber) {
+                setTimeout(function () {
+                    alert("You win!");
+                }, 100);
 
-                else if (counter > targetNumber) {
-                    setTimeout(function () { 
-                        alert("You loose!!");
-                    }, 100);
-                   
+                // window.location.reload()
+                wins++
+                // $("wins").text("Wins <br/>  " + wins)
+                document.getElementById("wins").innerHTML = "Wins <br/> " + wins;
+                $("#crystals").off("click", ".crystal-image")
 
-                    // window.location.reload()
-                    losses++
-                    // $("losses").text("Losses <br/>"  + losses)
-                    document.getElementById("losses").innerHTML = "Losses <br/>" + losses;
-                    $("#crystals").off("click", ".crystal-image")
-                }
+            }
+
+            else if (counter > targetNumber) {
+                setTimeout(function () {
+                    alert("You loose!!" );
+                }, 100);
+
+                losses++
+                document.getElementById("losses").innerHTML = "Losses <br/>" + losses;
+                $("#crystals").off("click", ".crystal-image")
+            }
 
 
 
-                //alerts are firing before the totals are logged
+            //alerts are firing before the totals are logged
+            // $("instructions").text("You have been sucked into a black hole!");
+             // $("losses").text("Losses <br/>"  + losses)
 
-            });
+        });
 
         // });
         //----------------------------------------------------------------------------  
